c6b836edd12618606f750beb02ba2910d3144599
Nhan Do <trongnhan19081994@gmail.com>
2021-09-25 22:08:50 +0700

    basic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css';
 import Greet from './components/Greet';
+import Person from './components/Person';
+import PersonList from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Trong',
+    last: 'Nhan'
+  }
+
+  const nameList = [
+    {
+      first: 'Trong',
+      last: 'Nhan'
+    },
+    {
+      first: 'Trong2',
+      last: 'Nhan2'
+    },
+    {
+      first: 'Trong2',
+      last: 'Nhan2'
+    },
+]
   return (
     <div className="App">
-      <Greet name='First component'/>
+      <Greet name='Trong Nhan' messageCount={10} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
---
+++ src/components/PersonList.tsx
@@ -0,0 +1,21 @@
+type PersonListProps = {
+    names: {
+        first: string,
+        last: string
+    }[]
+}
+
+const PersonList = (props: PersonListProps) => {
+    return (
+        <div>
+            {
+                props.names.map( (name, index) => (
+                    <h2 key={index}>{name.first} - {name.last}</h2>
+                ))
+            }
+            
+        </div>
+    )
+}
+
+export default PersonList
