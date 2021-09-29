e18e7558f2ac3ecafae666505ccb2687c6707945
Nhan Do <trongnhan19081994@gmail.com>
2021-09-29 21:03:43 +0700

    Polymorphic Components

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import List from './components/generics/List';
 import { RandomNumber } from './components/restriction/RandomNumber';
 import Toast from './components/templateliterals/Toast';
 import CustomButton from './components/html/Button';
+import Text from './components/polymorphic/Text';
 
 function App() {
   const personName = {
@@ -118,6 +119,10 @@ function App() {
       <CustomButton variant="primary" onClick={() => console.log('Clicked')}> 
         Primary Button 
       </CustomButton>
+      <br/>
+      <Text as="h1" size="lg">Heading</Text>
+      <Text as="p" size="md">Paragraph</Text>
+      <Text as="label" htmlFor="someID" size="sm" color="secondary">Label</Text>
     </div>
   );
 }
